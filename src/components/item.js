44051056e0e895b2b14cc9dafc10bc791fb52a10
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-11-08 14:44:51 +0700

    reformat code

--- src/components/item.js
+++ src/components/item.js
@@ -5,22 +5,30 @@ import Truncate from 'react-truncate';
 const { Meta } = Card;
 
 class ItemMovie extends Component {
-    render() {
-        const item = this.props.item
-        return <Card
-        onClick={() => {this.props.onItemMovieClick(item)}}
+  render() {
+    const item = this.props.item;
+    return (
+      <Card
+        onClick={() => {
+          this.props.onItemMovieClick(item);
+        }}
         hoverable
         style={{ width: 454 }}
-        cover={<img src={item.image_url}/>}
+        cover={<img src={item.image_url} />}
       >
         <Meta
           title={item.title}
           description={
-            <div><Truncate width='340' lines={2} ellipsis={<span>...</span>}>{item.overview}</Truncate></div>
+            <div>
+              <Truncate width="340" lines={2} ellipsis={<span>...</span>}>
+                {item.overview}
+              </Truncate>
+            </div>
           }
         />
       </Card>
-    }
+    );
+  }
 }
 
-export default ItemMovie
+export default ItemMovie;
--- src/components/list.js
+++ src/components/list.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { List } from 'antd';
-import ItemMovie from './item'
+import ItemMovie from './item';
 
 class ListMovie extends Component {
-    render() {
-        return <List 
+  render() {
+    return (
+      <List
         grid={{ gutter: 16, column: 4 }}
         dataSource={this.props.items}
         renderItem={item => (
-            <List.Item>
-                <ItemMovie item={item} onItemMovieClick={this.props.onItemMovieClick}/>
-            </List.Item>
-          )}>
-        </List>
-    }
+          <List.Item>
+            <ItemMovie
+              item={item}
+              onItemMovieClick={this.props.onItemMovieClick}
+            />
+          </List.Item>
+        )}
+      />
+    );
+  }
 }
 
-export default ListMovie
+export default ListMovie;
